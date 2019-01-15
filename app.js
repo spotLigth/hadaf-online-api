8c8f719ffcf039708da2e0ba34c366442f2573b8
z3by <zoaby.am@gmail.com>
2019-01-15 10:39:28 +0200

    FEAT setup cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors')
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -18,6 +19,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// apply cross origin resources sharing "CORS"
+app.use(cors({
+  origin: '*', // TODO change on production build
+  optionsSuccessStatus: 200
+ }));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
